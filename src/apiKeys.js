bfb49154d9308f19af3446140773f24db1189d37
Ravitejachatti <amma10701@gmail.com>
2023-04-28 02:16:14 +0530

    First Commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "d495692ab0427f9edb5a3a825cc74160",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
